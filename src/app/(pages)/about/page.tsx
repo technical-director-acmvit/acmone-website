e181f0d1d02bdedf3ee9af9b7b1dadb56bfde360
Van5sh <vansh05dhir@gmail.com>
2024-07-01 22:48:10 +0530

    DownloadPageadded

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,12 +1,8 @@
 import RootLayout from "@/app/layout";
-import About from "@/screens/AboutPage";
+
 
 const AboutPage = () => {
-  return (
-    <RootLayout>
-      <About />
-    </RootLayout>
-  );
+  
 };
 
 export default AboutPage;
--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,12 +1,8 @@
 import RootLayout from "@/app/layout";
-import About from "@/screens/AboutPage";
+
 
 const HomePage = () => {
-  return (
-    <RootLayout>
-      <About />
-    </RootLayout>
-  );
+
 };
 
 export default HomePage ;
