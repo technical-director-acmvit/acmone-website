09a0e00c0424e409e8ee68a8bc91bb3cff87b017
Supratim Ghose <supratim7ghose@gmail.com>
2024-07-04 03:48:08 +0530

    good to go...

--- src/app/components/Download/download.tsx
+++ src/app/components/Download/download.tsx
@@ -1,43 +1,40 @@
 import React from "react";
 import Navbar from "../Navbar";
+import Image from "next/image";
+import download from "@/app/assets/DownloadPage.svg";
 
 export default function DownLoad() {
-  return (
-    <div
-      className="bg-gray-900 text-white min-h-screen flex flex-col"
-      style={{
-        backgroundImage: "url('/DownloadPage.svg')",
-        backgroundPosition: "90% 55% ",
-        backgroundRepeat:"no-repeat",
-        backgroundSize: "26%",
-      }}
-    >
-      <div className="flex flex-col flex-grow">
-        <Navbar logoColor="white" textColor="white" />
-        <main className="flex-grow flex items-center">
-          <div className="mx-5 sm:ml-20 sm:mt-33 md:ml-30">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl mb-4">
-              Download the APK now!
-            </h1>
-            <div className="flex flex-col items-center space-y-4">
-              <button className="bg-white text-black px-4 py-2 sm:px-6 sm:py-2 rounded-full mb-4 hover:bg-green-200">
-                Download
-              </button>
-              <p className="text-gray-400 text-sm sm:text-base">
-                Version: 1.0.0.1
-              </p>
-              <p className="text-gray-400 text-sm sm:text-base">
-                For iOS and Android
-              </p>
+    return (
+        <div className="bg-gray-900 text-white min-h-screen flex flex-col">
+            <div className="flex flex-col justify-between flex-grow">
+                <Navbar logoColor="white" textColor="white" />
+                <main className="w-[100vw] justify-around flex lg:flex-row-reverse md:flex-row flex-col items-center h-[100%]">
+                    <Image
+                        src={download}
+                        alt="download"
+                        className="lg:w-[30%] md:w-[35%] w-[70%]"
+                    />
+                    <div className="flex flex-col justify-between items-center mx-5 sm:ml-20  md:ml-30 mt-[5%]">
+                        <h1 className="lg:text-4xl text-xl md:text-2xl mb-4">
+                            Download the APK now!
+                        </h1>
+                        <div className="flex flex-col items-center space-y-4">
+                            <button className="bg-white text-black px-4 py-2 sm:px-6 sm:py-2 rounded-full mb-4 hover:bg-green-200">
+                                Download
+                            </button>
+                            <p className="text-gray-400 text-sm sm:text-base">
+                                Version: 1.0.0.1
+                            </p>
+                            <p className="text-gray-400 text-sm sm:text-base">
+                                For iOS and Android
+                            </p>
+                        </div>
+                    </div>
+                </main>
+                <p className="text-gray-400 sm:mt-10 lg:text-lg text-base  self-center my-4">
+                    Made with ♥ by ACM-VIT
+                </p>
             </div>
-          </div>
-        </main>
-        <footer className="flex justify-center mb-8">
-          <p className="text-gray-400 text-xs sm:mt-10 sm:text-sm md:text-base">
-            Made with ♥ by ACM-VIT
-          </p>
-        </footer>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- src/app/components/Home/home.tsx
+++ src/app/components/Home/home.tsx
@@ -1,31 +1,47 @@
 import React from "react";
 import Navbar from "../Navbar";
+import Image from "next/image";
+import Team from "@/app/assets/TeamBrainstorming.svg";
+import Logo from "@/app/assets/LogoB.svg";
 
 const Home: React.FC = () => {
-  return (
-    <div className='name="viewport" content="width=device-width, initial-scale=1.0'>
-      <div
-        className="min-h-screen sm:h-screen bg-white sm:bg-fixed bg-no-repeat flex sm:flex-col flex-col"
-        style={{
-          backgroundImage:
-            "url('/PinkSpreadPurpleStripes.svg'), url('/GreenDotsPurpleCircles.svg'), url('/TeamBrainstorming.svg'), url('/LogoB.svg')",
-          backgroundPosition:
-            "top 111.5% right 116%, left 107% top 40%, center top 13.5%, center top 62%",
-          backgroundSize: "24%, 18%, 22.5%, 19%",
-        }}
-      >
-        <Navbar textColor="black" logoColor="black" />
-        <div className="flex flex-col flex-1 justify-end items-center sm:text-center font-serif pb-20">
-          <h1 className="text-black text-3xl md:text-4xl">ACM IN ONE</h1>
-          <button className="mt-5 px-6 py-1 bg-black text-white text-lg md:text-xl rounded-lg">
-            <a href="/download" className="px-8 py-2 block">
-              Download
-            </a>
-          </button>
+    return (
+        <div className='name="viewport" content="width=device-width, initial-scale=1.0'>
+            <div
+                className="min-h-screen sm:h-screen bg-white sm:bg-fixed bg-no-repeat flex sm:flex-col flex-col"
+                style={{
+                    backgroundImage:
+                        "url('/PinkSpreadPurpleStripes.svg'), url('/GreenDotsPurpleCircles.svg')",
+                    backgroundPosition:
+                        "top 111.5% right 116%, left 107% top 40%, center top 13.5%, center top 62%",
+                    backgroundSize: "24%, 18%, 22.5%, 19%",
+                }}
+            >
+                <Navbar textColor="black" logoColor="black" />
+                <div className="flex flex-col flex-1 justify-around items-center sm:text-center font-serif pb-20">
+                    <Image
+                        src={Team}
+                        alt="Team Brainstorming"
+                        className="lg:w-[20%] md:w-[40%] w-[60%] my-4"
+                    />
+                    <Image
+                        src={Logo}
+                        alt="ACM1"
+                        className="lg:w-[20%] md:w-[40%] w-[60%] my-4"
+                    />
+
+                    <h1 className="text-black text-3xl md:text-4xl">
+                        ACM IN ONE
+                    </h1>
+                    <button className="mt-5 px-6 py-1 bg-black text-white text-lg md:text-xl rounded-lg">
+                        <a href="/download" className="px-8 py-2 block">
+                            Download
+                        </a>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
