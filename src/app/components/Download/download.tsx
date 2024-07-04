40a84d5cbc201d7dd88dd9c0488cea3edb4f5b1f
Supratim Ghose <supratim7ghose@gmail.com>
2024-07-04 20:49:53 +0530

    release apk

--- src/app/components/Download/download.tsx
+++ src/app/components/Download/download.tsx
@@ -1,9 +1,17 @@
+"use client";
 import React from "react";
 import Navbar from "../Navbar";
 import Image from "next/image";
 import download from "@/app/assets/DownloadPage.svg";
+import { saveAs } from "file-saver";
 
 export default function DownLoad() {
+    const downloadFile = () => {
+        saveAs(
+            "https://github.com/technical-director-acmvit/acmone-website/releases/download/v1.0.0/ACMOne.apk",
+            "ACMOne.apk"
+        );
+    };
     return (
         <div className="bg-gray-900 text-white min-h-screen flex flex-col">
             <div className="flex flex-col justify-between flex-grow">
@@ -19,7 +27,10 @@ export default function DownLoad() {
                             Download the APK now!
                         </h1>
                         <div className="flex flex-col items-center space-y-4">
-                            <button className="bg-white text-black px-4 py-2 sm:px-6 sm:py-2 rounded-full mb-4 hover:bg-green-200">
+                            <button
+                                onClick={downloadFile}
+                                className="bg-white text-black px-4 py-2 sm:px-6 sm:py-2 rounded-full mb-4 hover:bg-green-200"
+                            >
                                 Download
                             </button>
                             <p className="text-gray-400 text-sm sm:text-base">
